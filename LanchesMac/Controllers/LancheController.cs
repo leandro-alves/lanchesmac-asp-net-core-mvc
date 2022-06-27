6e3b9a36274d727bc07f359eb210ce77b9b88d69
Leandro Henrique <leandro2600@gmail.com>
2022-06-27 12:10:30 -0300

    Incluindo controller e view para listagem de lanches

---
+++ LanchesMac/Controllers/LancheController.cs
@@ -0,0 +1,25 @@
+﻿using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _lancheRepository.Lanches; 
+            return View(lanches);
+        }
+    }
+}
