bfd83d567fb7df219af3dabbc9f21d97b9a6d163
Leandro Henrique <leandro2600@gmail.com>
2022-07-05 00:05:49 -0300

    Ajustando exibição dos lanches e apresentação do carrinho que estava distorçida

--- LanchesMac/Controllers/LancheController.cs
+++ LanchesMac/Controllers/LancheController.cs
@@ -45,5 +45,11 @@ namespace LanchesMac.Controllers
 
             return View(lanchesListViewModel);
         }
+
+        public IActionResult Details(int lancheId)
+        {
+            var lanche = _lancheRepository.Lanches.FirstOrDefault(l => l.LancheId == lancheId);
+            return View(lanche);
+        }
     }
 }
