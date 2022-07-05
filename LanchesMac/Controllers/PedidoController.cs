6fa4314495b679bbc6c031e41583a83252a71a8b
Leandro Henrique <leandro2600@gmail.com>
2022-07-05 13:49:59 -0300

    Criando o método Action Checkout HttpPost

--- LanchesMac/Controllers/PedidoController.cs
+++ LanchesMac/Controllers/PedidoController.cs
@@ -23,7 +23,48 @@ namespace LanchesMac.Controllers
         [HttpPost]
         public IActionResult Checkout(Pedido pedido)
         {
-            return View();
+            int totalItensPedido = 0;
+            decimal precoTotalPedido = 0.0m;
+
+            //Obtém os itens do carrinho de compras do cliente
+            List<CarrinhoCompraItem> itens = _carrinhoCompra.GetCarrinhoCompraItens();
+            _carrinhoCompra.CarrinhoCompraItens = itens;
+
+            //Verifica se existem itens de pedido
+            if (_carrinhoCompra.CarrinhoCompraItens.Count == 0)
+            {
+                ModelState.AddModelError("", "Seu carrinho está vazio, que tal incluir um lanche.");
+            }
+
+            //Calcula o total de itens e o valor total do pedido
+            foreach(var item in itens)
+            {
+                totalItensPedido += item.Quantidade;
+                precoTotalPedido += (item.Lanche.Preco * item.Quantidade);
+            }
+
+            //Atribui os valores obtidos ao pedido
+            pedido.TotalItensPedido = totalItensPedido;
+            pedido.PedidoTotal = precoTotalPedido;
+
+            //Valida os dados do pedido
+            if (ModelState.IsValid)
+            {
+                //Cria o pedido
+                _pedidoRepository.CriarPedido(pedido);
+
+                //Define mensagens ao cliente
+                ViewBag.CheckoutCompletoMensagem = "Obrigado pelo seu pedido :)";
+                ViewBag.TotalPeido = _carrinhoCompra.GetCarrinhoCompraTotal();
+
+                //Limpa o carrinho do cliente
+                _carrinhoCompra.LimparCarrinho();
+
+                //Exibe a View com os dados do cliente e do pedido
+                return View("~/Views/Pedido/CheckoutCompleto.cshtml", pedido);
+            }
+
+            return View(pedido);
         }
     }
 }
