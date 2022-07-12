06761bd3bebe8d72e05550b3c1c730be720e58f9
Leandro Henrique <leandro2600@gmail.com>
2022-07-12 13:25:08 -0300

    Criando a view AccessDenied e o menu para o usuário Admin

--- LanchesMac/Controllers/AccountController.cs
+++ LanchesMac/Controllers/AccountController.cs
@@ -91,5 +91,10 @@ namespace LanchesMac.Controllers
             await _signInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
         }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
     }
 }
