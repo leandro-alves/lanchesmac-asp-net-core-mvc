b455082eb6064ac698f1984651967ab9b7caf24e
Leandro Henrique <leandro2600@gmail.com>
2022-07-23 00:19:00 -0300

    Implementação do relatório de vendas por data

--- LanchesMac/Startup.cs
+++ LanchesMac/Startup.cs
@@ -1,4 +1,5 @@
-﻿using LanchesMac.Context;
+﻿using LanchesMac.Areas.Admin.Services;
+using LanchesMac.Context;
 using LanchesMac.Models;
 using LanchesMac.Repositories;
 using LanchesMac.Repositories.Interfaces;
@@ -32,6 +33,7 @@ public class Startup
         services.AddTransient<ICategoriaRepository, CategoriaRepository>();
         services.AddTransient<IPedidoRepository, PedidoRepository>();
         services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
+        services.AddScoped<RelatorioVendasService>();
 
         services.AddAuthorization(options =>
         {
