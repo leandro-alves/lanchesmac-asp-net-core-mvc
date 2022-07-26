5c072c53664c9d1fe8b0476e0b8cab24477a9b8c
Leandro Henrique <leandro2600@gmail.com>
2022-07-25 23:46:54 -0300

    Pequenos ajustes

---
+++ LanchesMac/Areas/Admin/Controllers/AdminImagensController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Areas.Admin.Controllers
+{
+    public class AdminImagensController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
